9f02a35e11e2e2ac7d4e66f0d06a1ec2f730183f
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2022-04-23 23:09:13 +0200

    Ex 1.6 OK

---
+++ ex_1_6_StringCompression.py
@@ -0,0 +1,35 @@
+# String compression : Implement a method to perform basic string compression using the counts of repeated characters.
+# For example, the string "aabccccaaaa" would become "a2b1c5a3". If the "compressed" string would not become smaller
+# than the original string,"" your method should return the original string. You can assume the string has only uppercase
+# and lower case letters (a-z)
+
+def compression(string):
+    compressed = [0] * len(string)
+    successive_counter = 1
+    compressed_index = 0
+    if len(string) <= 2: ## only useful to compress if we have 3-letter string
+        return string
+    for i in range(1, len(string) + 1):
+        if i < len(string) and string[i] == string[i-1]:
+            successive_counter += 1
+        else:
+            compressed[compressed_index] = string[i-1]
+            compressed_index += 1
+            if successive_counter > 1:
+                for digit in str(successive_counter):
+                    compressed[compressed_index] = digit
+                    compressed_index += 1
+                successive_counter = 1
+    compressed_size = compressed_index
+    if compressed_size < len(string):
+        return "".join(compressed[0:compressed_size])
+    else:
+        return string
+
+assert compression("abcde") == "abcde"
+assert compression("a") == "a"
+assert compression("aa") == "aa"
+assert compression("aaa") == "a3"
+assert compression("aabbcc") == "aabbcc"
+assert compression("aabbccc") == "a2b2c3"
+assert compression("a"*15+"b"*5) == "a15b5"
