bdd11d784481c51427a3ee353bf0e5d3314198f5
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2022-04-24 12:45:03 +0200

    1.8 : Adds comments

--- ex_1_8_zero_matrix.py
+++ ex_1_8_zero_matrix.py
@@ -28,25 +28,25 @@ def zero_matrix_opti(matrix):
     n = 0
     if m > 0:
         n = len(matrix[0])
-    first_row_to_erase = 0 in matrix[0]
-    first_column_to_erase = 0 in [matrix[i][0] for i in range(m)]
+    first_row_to_erase = 0 in matrix[0] # we will use the first row to mark rows to delete, so we need to remember the state of it
+    first_column_to_erase = 0 in [matrix[i][0] for i in range(m)] # idem for first column
     for i in range(1,m):
         for j in range(1,n):
-            if matrix[i][j] == 0:
+            if matrix[i][j] == 0: # if element = 0, we mark the first column and row for future deletion
                 matrix[i][0] = 0
                 matrix[0][j] = 0
-    for i in range(1,m):
-        if matrix[i][0] == 0:
+    for i in range(1,m): # we will erase first row/column after the rest of the matrix
+        if matrix[i][0] == 0: # if row marked for deletion
             for j in range(1,n):
                 matrix[i][j] = 0
     for j in range(1,n):
-        if matrix[0][j] == 0:
-            for i in range(1,m):
+        if matrix[0][j] == 0: # we will erase first row/column after the rest of the matrix
+            for i in range(1,m): # if column marked for deletion
                 matrix[i][j] = 0
-    if first_column_to_erase:
+    if first_column_to_erase: # now we can put 0 in the first column without altering the rest
         for i in range(m):
             matrix[i][0] = 0
-    if first_row_to_erase:
+    if first_row_to_erase: # now we can put 0 in the first row without altering the rest
         for j in range(n):
             matrix[0][j] = 0
 
