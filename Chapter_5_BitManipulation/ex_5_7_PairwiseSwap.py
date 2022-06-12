feffb8de8b3b81680a96d6d191183fff10a94629
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2022-06-12 16:43:23 +0200

    Ex 5.7 OK

---
+++ Chapter_5_BitManipulation/ex_5_7_PairwiseSwap.py
@@ -0,0 +1,22 @@
+# 5.7 Pairwise swap
+# Write a program to swap odd and even bits in an integer with as few instructions as
+# possible (e.g., bit O and bit 1 are swapped, bit 2 and bit 3 are swapped, and so on).
+# ex :
+# 101010 => 010101
+# 000000 => 000000
+# 111111 => 111111
+# ABCDEF & 101010 = A0C0E0 >>1= 0A0C0E
+import unittest
+
+
+def pairwise_swap(n):
+    assert n < 2**32
+    mask = 0b10101010101010101010101010101010
+    a = (mask & n) >> 1
+    b = (n << 1) & n
+    return a | b
+
+
+class TestCase(unittest.TestCase):
+    def test_pairwise_swap(self):
+        self.assertEqual(85, pairwise_swap(170))
