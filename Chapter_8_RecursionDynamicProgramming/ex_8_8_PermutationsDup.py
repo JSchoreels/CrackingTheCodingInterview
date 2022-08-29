8c848476ae67b5895341a663d310d038245686c2
Jonathan Schoreels <Jonathan.Schoreels@gmail.com>
2022-08-29 20:46:18 +0200

    Ex 8.7 8.8 OK

---
+++ Chapter_8_RecursionDynamicProgramming/ex_8_8_PermutationsDup.py
@@ -0,0 +1,16 @@
+def permutation(string : str):
+    if len(string) == 1:
+        return [string]
+    else:
+        first_char = string[0]
+        rest = string[1:]
+        rec_permutation = permutation(rest)
+        result = set() # The set will allow dups to be filtered automatically
+        for perm in rec_permutation:
+            for i in range(len(perm)+1):
+                new_perm = perm[:i] + first_char + perm[i:]
+                result.add(new_perm)
+        print(f"result : {result}")
+        return result
+
+print(permutation("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
